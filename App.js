4774d75d46356c0a00e5931d9f5189371dd06580
notRly <nvkasyanov@gmail.com>
2018-04-13 14:02:53 +0300

    Бекенд сбора данных заказа

--- App.js
+++ App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {Button, Link} from './src/ui';
 
+import Order from './src/order';
+
 export default class App extends React.Component {
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Оппппа!</Text>
         <Button>Кнопуля</Button>
         <Link>Ссылка</Link>
+        <Order orderId="12860261" />
       </View>
     );
   }
