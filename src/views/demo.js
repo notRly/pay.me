70c5545a49bd30a0ca501ee247a5de86a9d16935
Klokov Anton <klokovas@eruditor-group.com>
2018-04-13 16:03:40 +0300

    UI. контейнер в демо

--- src/views/demo.js
+++ src/views/demo.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Link, Title, Hint, Input} from '../ui';
-import {Button, Text} from 'native-base';
+import {Button, Text, Container} from 'native-base';
 
 export default class Demo extends React.Component {
   static navigationOptions = {
@@ -15,7 +15,7 @@ export default class Demo extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <Container>
         <Title>Заголовок</Title>
           <Button onPress={this.goToOrderScreen}>
               <Text>Click Me! </Text>
@@ -24,14 +24,7 @@ export default class Demo extends React.Component {
         <Hint color="red">Красный текст</Hint>
         <Hint color="black">Черный текст текст</Hint>
         <Input />
-      </View>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
