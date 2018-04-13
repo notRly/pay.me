82a9de3630c56c0ccfcc41dbad1b8576c966f8a6
Olga Stogova <helgastogova@gmail.com>
2018-04-13 20:42:20 +0300

    иконки

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import {Root} from "native-base";
+import {Root} from 'native-base';
 import Navigation from './src/navigation';
-
+import {Font} from 'expo';
 export default class App extends React.Component {
+  componentDidMount() {
+    Font.loadAsync({
+      Ionicons: require('./assets/fonts/ionicons.ttf'),
+    });
+  }
   render() {
     return (
       <Root>
-        <Navigation/>
+        <Navigation />
       </Root>
     );
   }
--- src/views/paymentType.js
+++ src/views/paymentType.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {StyleSheet} from 'react-native';
+
 import {
   StyleProvider,
   Button,
@@ -10,6 +11,7 @@ import {
   Content,
   CheckBox,
   H2,
+  Icon,
 } from 'native-base';
 import getTheme from '../../native-base-theme/components/';
 import theme from '../../native-base-theme/variables/platform';
@@ -29,6 +31,7 @@ export default class PaymentType extends React.Component {
         <Container>
           <Content style={styles.content} withPadding>
             <H2 style={styles.title2}>Выберите способ оплаты</H2>
+            <Icon name="logo-apple" />
             {Object.keys(PAYMENT_TYPES).map(paymentType => {
               // TODO: впендошить иконку
               if (paymentType === PAYMENT_TYPES.APPLE_PAY)
