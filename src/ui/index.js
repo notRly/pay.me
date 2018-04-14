fb5f091b2e594537d178e28eabc34eb3d5604366
Smolyanov Igor <smolyan_91@mail.ru>
2018-04-14 10:05:46 +0300

    Удаление creditcards

--- src/ui/index.js
+++ src/ui/index.js
@@ -4,4 +4,3 @@ export {default as Link} from './link';
 export {default as Title} from './title';
 export {default as Hint} from './hint';
 export {default as Input} from './input';
-export {default as CreditCards} from './creditcards';
