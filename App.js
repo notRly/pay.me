76bf288f04f5e86de4490aaebe6a19d97baf0468
notRly <nvkasyanov@gmail.com>
2018-04-14 10:49:25 +0300

    deeplinking try

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
 console.disableYellowBox = true;
+
 import React from 'react';
 import {Root} from 'native-base';
 import Navigation from './src/navigation';
-import {Font} from 'expo';
+import {Font, Constants} from 'expo';
+import {Linking} from 'react-native';
 export default class App extends React.Component {
-  componentDidMount() {
+
+_handleUrl = (url) => {
+  this.setState({ url });
+
+  alert(Constants.linkingUri);
+  alert(url);
+  let queryString = url.replace(Constants.linkingUri, '');
+  if (queryString) {
+    let data = qs.parse(queryString);
+    alert(`Linked to app with data: ${JSON.stringify(data)}`);
+  }
+}
+
+  async componentDidMount() {
     Font.loadAsync({
       Ionicons: require('./assets/fonts/ionicons.ttf'),
     });
+
+    const initialUrl = await Linking.getInitialURL();
+    alert("initialUrl", initialUrl);
+
+    Linking.addEventListener('url', this._handleUrl)
   }
   render() {
     return (
